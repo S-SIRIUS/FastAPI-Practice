4e689eb3a2ddb8de78c8d0e1ed90974aa1db29b9
S-SIRIUS <sirius5b8b@gmail.com>
2024-03-01 01:15:18 +0900

    [feat] Nav bar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,7 +3,7 @@
   import Home from "./routes/Home.svelte"
   import Detail from "./routes/Detail.svelte"
   import QuestionCreate from "./routes/QuestionCreate.svelte"
-
+  import Navigation from './components/Navigation.svelte';
   const routes = {
     '/':Home,
     '/detail/:question_id': Detail,
@@ -11,4 +11,5 @@
   }
 </script>
 
+<Navigation/>
 <Router {routes}/>
