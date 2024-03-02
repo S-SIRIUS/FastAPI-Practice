e5d1d8d4964d998a547c7dfa174f777e506fb846
S-SIRIUS <sirius5b8b@gmail.com>
2024-03-02 14:12:16 +0900

    [feat] search

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -1,15 +1,31 @@
 from datetime import datetime
 from domain.question.question_schema import QuestionCreate, QuestionUpdate
 
-from models import Question, User
+from sqlalchemy import and_
+from models import Question, User, Answer
 from sqlalchemy.orm import Session
 
 
 # skip은 시작, limit은 데이터의 건수
-def get_question_list(db:Session, skip:int =0, limit: int = 10):
-    _question_list = db.query(Question).order_by(Question.create_date.desc())
-    total = _question_list.count()
-    question_list = _question_list.offset(skip).limit(limit).all() # 추리기
+def get_question_list(db:Session, skip:int =0, limit: int = 10, keyword: str=''):
+    question_list = db.query(Question)
+    # 키워드 있으면 검색기능
+    if keyword:
+        search = '%%{}%%'.format(keyword)
+        sub_query = db.query(Answer.question_id, Answer.content, User.username) \
+            .outerjoin(User, and_(Answer.user_id == User.id)).subquery()
+        question_list = question_list \
+            .outerjoin(User) \
+            .outerjoin(sub_query, and_(sub_query.c.question_id == Question.id)) \
+            .filter(Question.subject.ilike(search) |  # 질문제목
+                    Question.content.ilike(search) |  # 질문내용
+                    User.username.ilike(search) |  # 질문작성자
+                    sub_query.c.content.ilike(search) |  # 답변내용
+                    sub_query.c.username.ilike(search)  # 답변작성자
+                    )
+    total = question_list.distinct().count()
+    question_list = question_list.order_by(Question.create_date.desc()) \
+        .offset(skip).limit(limit).distinct().all()
     return total, question_list
 
 def get_question(db:Session, question_id:int):
